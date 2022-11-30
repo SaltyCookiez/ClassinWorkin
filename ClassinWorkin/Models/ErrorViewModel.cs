813c5c9aa8d15688a40cfde5d1393ec2babfa358
opilane <opilane@FUJI-YMHP016307>
2022-11-30 12:33:26 +0200

    Add project files.

---
+++ ClassinWorkin/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ClassinWorkin.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ ClassinWorkin/Models/Youtuber.cs
@@ -0,0 +1,12 @@
+﻿namespace ClassinWorkin.Models
+{
+    public class Youtuber
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Nickname { get; set; }
+        public int Income { get; set; }
+
+        public int Subscribers { get; set; }
+    }
+}
